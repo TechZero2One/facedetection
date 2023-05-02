49af38a5c12b0d431a249d3d5ec4b3b4947b87f8
Bibek Borah <bibek.bora@gmail.com>
2023-05-02 21:06:43 +0100

    Before image component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      input: ''
+    } 
+  }
+
+ 
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
+  render() {
+
+     const config = {
+      particles: {
+        color: {
+          value: "#a13f23",
+        },
+        links: {
+          color: "#098712",
+        }
+      },
+      background: {
+        color: {
+          value: "#121",
+        }
+      }
+    };
+    
+      
+      return (
+        <div className="App">
+            <ParticlesBg color="#ffffff" num={100} config={config} type="cobweb" bg={true} />
+            {/* <ParticlesBg color="#ffffff" num={100} type="cobweb" bg={true} /> */}
+            <Navigation />
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm onInputChange={this.onInputChange}/>
+            {/* {
+              onInputChange={this.onInputChange}
+                  <FaceRecognition />} */}
+
+        </div>
+     );
+  } 
 }
+
 
 export default App;
